e5deaa3178813075c5e25ef5b28b4c8895b1e1fb
humbug <mail.raczzoltan@gmail.com>
2024-04-18 00:32:26 +0200

    better installation guide 2

--- service-worker.js
+++ service-worker.js
@@ -8,11 +8,11 @@ const urlsToCache = [
   'jquery-3.5.1.min.js',
   'crypto-js.min.js',
   'carrypass.js',
-  // Add more files you want to cache here
+  
 ];
 
 self.addEventListener('install', function(event) {
-  // Perform install steps
+  
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
